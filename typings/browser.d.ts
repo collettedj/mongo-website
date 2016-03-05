711b36ba3d310a6b5adebd9423901aea8a09179c
David Collette <collettedj@gmail.com>
2016-03-05 08:40:48 -0800

    start adding typescript to mongo website

---
+++ typings/browser.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="browser/ambient/gulp-nodemon/gulp-nodemon.d.ts" />
+/// <reference path="browser/ambient/gulp/gulp.d.ts" />
+/// <reference path="browser/ambient/node/node.d.ts" />
+/// <reference path="browser/ambient/orchestrator/orchestrator.d.ts" />
+/// <reference path="browser/ambient/q/q.d.ts" />
---
+++ typings/main.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="main/ambient/gulp-nodemon/gulp-nodemon.d.ts" />
+/// <reference path="main/ambient/gulp/gulp.d.ts" />
+/// <reference path="main/ambient/node/node.d.ts" />
+/// <reference path="main/ambient/orchestrator/orchestrator.d.ts" />
+/// <reference path="main/ambient/q/q.d.ts" />
